a88a33cf077776301200b0bd552a835e57572781
rhannequin <remy.hannequin@gmail.com>
2013-07-14 23:23:19 +0200

    [docs] Improve Sun doc

--- public/assets/scripts/docs.js
+++ public/assets/scripts/docs.js
@@ -2,14 +2,21 @@
   'use strict';
 
   var $attributes = $('.attribute');
-  $attributes.find('h4').on('click', function () {
-    var $parent = $(this).parent();
-    var $children = $parent.find('> .attribute');
+  var $buttons = $attributes.find('button');
+  var $h4 = $attributes.find('h4');
+  var clickHandler = function() {
+    var $mainParent = $(this).parents('.attribute');
+    var $children = $mainParent.find('> .attribute');
+    var $buttonIcon = $mainParent.find('button').find('i');
     if($children.is(':visible')) {
       $children.hide();
+      $buttonIcon.removeClass('icon-chevron-down').addClass('icon-chevron-right');
     } else {
       $children.show();
+      $buttonIcon.removeClass('icon-chevron-right').addClass('icon-chevron-down');
     }
-  });
+  };
+  $h4.on('click', clickHandler);
+  $buttons.on('click', clickHandler);
 
 })(jQuery);
