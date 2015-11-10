19425f9d809e1d0a0affe55bec4416541a11c7ff
Rémy Hannequin <remy.hannequin@gmail.com>
2015-11-10 13:59:42 +0100

    Remove #id, #slug, #created_at and #updated_at Planet's attributes from rendering

--- app/controllers/api/v1/planets_controller.rb
+++ app/controllers/api/v1/planets_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class PlanetsController < ApplicationController
       def index
-        render json: Planet.all.to_json
+        render json: Planet.all.to_json(except: %i( id slug created_at updated_at ))
       end
     end
   end
--- spec/requests/api/v1/planets_spec.rb
+++ spec/requests/api/v1/planets_spec.rb
@@ -5,8 +5,25 @@ describe 'List of Planets', type: :request do
     @planets = create_list :planet, 2
   end
 
-  it 'responds successfully with an HTTP 200 status code' do
+  it 'provides correct number of planets' do
     get '/api/v1/planets'
-    expect(json.to_json).to eq(@planets.to_json)
+    expect(json.size).to eq(2)
+  end
+
+  it 'excludes technical fields' do
+    get '/api/v1/planets'
+    first_planet = json.first.to_json
+    expect(first_planet).to have_json_path('name')
+    expect(first_planet).not_to have_json_path('id')
+    expect(first_planet).not_to have_json_path('slug')
+    expect(first_planet).not_to have_json_path('created_at')
+    expect(first_planet).not_to have_json_path('updated_at')
+  end
+
+  it 'has created values' do
+    get '/api/v1/planets'
+    first_planet_created = @planets.first
+    first_planet_rendered = json.first
+    expect(first_planet_rendered['name']).to eq(first_planet_created.name)
   end
 end
